6579c3410e1ebf185270b5ba9b6469fc00dad922
Jermiah Joseph <jermiahjoseph98@gmail.com>
2025-05-20 11:39:50 -0400

    feat: Add pypi-dependencies section and update example notebook and script to use `dirs` object

--- TEMPLATE/workflow/scripts/example_script.py
+++ TEMPLATE/workflow/scripts/example_script.py
@@ -1,6 +1,7 @@
 import logging
-import os
 from pathlib import Path
+
+from damply import dirs
 
 logging.basicConfig(
 	level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s'
@@ -10,16 +11,21 @@ logger = logging.getLogger(__name__)
 
 
 def main() -> None:
-	# examples of template defined env variables
-	env_dir_variables = [
-		'RAWDATA',
-		'PROCDATA',
-		'SCRIPTS',
-	]
+	print(f'{dirs.PROJECT_ROOT=}')
 
-	for var in env_dir_variables:
-		var_path = Path(os.environ.get(var))
-		logger.info(f'{var}: {var_path} has {len(list(var_path.glob("*")))} files')
+	print(f'{dirs.RAWDATA=} has {len(list(dirs.RAWDATA.glob("*")))} files')
+	print(f'{dirs.PROCDATA=} has {len(list(dirs.PROCDATA.glob("*")))} files')
+	print(f'{dirs.SCRIPTS=} has {len(list(dirs.SCRIPTS.glob("*")))} files')
+
+	# these are all available via the `dirs`` object
+	# CONFIG       : ├── config
+	# LOGS         : ├── logs
+	# METADATA     : ├── metadata
+	# NOTEBOOKS    : ├── workflow/notebooks
+	# PROCDATA     : ├── data/procdata
+	# RAWDATA      : ├── data/rawdata
+	# RESULTS      : ├── data/results
+	# SCRIPTS      : └── workflow/scripts
 
 
 if __name__ == '__main__':
